73c8cc1c6f1a53a922a157092fa73a8d82ad8886
JaeJaeC00L <65375884+JaeJaeC00L@users.noreply.github.com>
2022-06-27 23:01:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 #anti-procrastination-together
+#user input subject, type of work, and amount of question for assignment and program outputs time needed estimation. 
+#PRogram can learn from past habits and estimate more accurate.
