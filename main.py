56e57aa93f37128470df196724a5c0a3f814ba60
disobliged <80568486+disobliged@users.noreply.github.com>
2022-06-27 23:39:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-#anti-procrastination-together
-#user input subject, type of work, and amount of question for assignment and program outputs time needed estimation. 
-#PRogram can learn from past habits and estimate more accurate.
+# Homework Calculator
